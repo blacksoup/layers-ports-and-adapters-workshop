8d2ea128abdaa93abed3934f48cc6486a6bc3108
Adam Pigler <adam.pigler@escalion.com>
2020-09-17 15:39:39 +0000

    [docker_workshop] Fixed deptrac issues

--- bin/deptrac.bat
+++ bin/deptrac.bat
@@ -1,3 +1,3 @@
 @echo off
 
-call docker-compose run --rm php deptrac analyze --formatter-graphviz-dump-image=var/dependency-graph.png
+call docker-compose run --rm php deptrac analyze --formatter-graphviz=true --formatter-graphviz-dump-image=var/dependency-graph.png
